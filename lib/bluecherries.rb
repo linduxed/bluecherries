fe22093d6d58ced0c0109016b1dd3dd12cc04d8e
linduxed <linduxed@gmail.com>
2013-06-28 18:27:05 +0200

    Add the 'motions' method to Layout.
    'motions' returns a placeholder array of Motion.

--- lib/bluecherries.rb
+++ lib/bluecherries.rb
@@ -13,12 +13,51 @@ module BlueCherries
       @keys = layout_file.readlines.map(&:chomp)
     end
 
+    def motions
+      left_hand_motions = left_index_finger_motions
+      right_hand_motions = right_index_finger_motions
+
+      return left_hand_motions + right_hand_motions
+    end
+
     private
 
     attr_reader :keys, :layout
 
     def layout_path(name)
       File.expand_path("../../layouts/#{name}.layout", __FILE__)
+    end
+
+    def right_index_finger_motions
+      [
+        Motion.new(:y),
+        Motion.new(:h),
+        Motion.new(:n),
+        Motion.new(:u),
+        Motion.new(:j),
+        Motion.new(:m)
+      ]
+    end
+
+    def left_index_finger_motions
+      [
+        Motion.new(:t),
+        Motion.new(:g),
+        Motion.new(:b),
+        Motion.new(:r),
+        Motion.new(:f),
+        Motion.new(:v)
+      ]
+    end
+  end
+
+  # Defines the different motions that one can do with one hand that result in
+  # a sequence of pressed keys.
+  class Motion
+    attr_reader :keys
+
+    def initialize(keys)
+      @keys = keys
     end
   end
 end
