a48ccf3e206fc29c4ca7854f7bb65532833e3fa4
linduxed <linduxed@gmail.com>
2013-06-28 15:14:15 +0200

    Add Layout class and a layout file.

--- lib/bluecherries.rb
+++ lib/bluecherries.rb
@@ -1,5 +1,24 @@
 require "bluecherries/version"
 
 module BlueCherries
-  # Your code goes here...
+  class Layout
+    def initialize(layout = :qwerty)
+      begin
+        layout_file = File.open layout_path(layout.to_s)
+      rescue Errno::ENOENT
+        puts "ERROR: The layout file for #{layout.to_s.upcase} doesn't exist."
+        exit
+      end
+
+      @keys = layout_file.readlines.map(&:chomp)
+    end
+
+    private
+
+    attr_reader :keys, :layout
+
+    def layout_path(name)
+      File.expand_path("../../layouts/#{name}.layout", __FILE__)
+    end
+  end
 end
