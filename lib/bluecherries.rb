587e7290e9926ad9b869edc8a0330a481ae4831e
linduxed <linduxed@gmail.com>
2013-07-04 15:12:32 +0200

    Refactor Key#initialize.

--- lib/bluecherries.rb
+++ lib/bluecherries.rb
@@ -66,14 +66,19 @@ module BlueCherries
     attr_reader :char, :hand, :row_number
 
     def initialize(char, hand, row_number)
+      self.hand = hand
+      @char = char
+      @row_number = row_number
+    end
+
+    private
+
+    def hand=(hand)
       if hand == :left || hand == :right
         @hand = hand
       else
         raise BadHandError
       end
-
-      @char = char
-      @row_number = row_number
     end
   end
 
