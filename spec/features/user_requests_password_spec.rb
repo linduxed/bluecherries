1c6b3a244b71dc81b9c534f4bf3bd64c9b167e64
linduxed <linduxed@gmail.com>
2013-07-11 15:55:53 +0200

    Wrote failing high-level test.

---
+++ spec/features/user_requests_password_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Requesting a password' do
+  describe 'without any arguments' do
+    let(:generated_passwords) { run_app.readlines }
+
+    it 'returns passwords with a default format' do
+      expect(generated_passwords).to have(6).items
+      generated_passwords.each do |password|
+        expect(password).to be_longer_than(15)
+        expect(password).to have_title_case
+      end
+    end
+
+    def run_app
+      IO.popen(File.expand_path('../../../bin/bluecherries', __FILE__))
+    end
+  end
+end
