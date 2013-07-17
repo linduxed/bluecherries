fbf99ed5e3c78bee492fe60d2d9f6e03a320e06c
linduxed <linduxed@gmail.com>
2013-07-17 15:17:39 +0200

    Refactor Dictionary.

--- lib/bluecherries/dictionary.rb
+++ lib/bluecherries/dictionary.rb
@@ -14,8 +14,7 @@ module BlueCherries
     private
 
     def extract_words_from_dictionary_file
-      dictionary = File.readlines(dictionary_path).map(&:chomp)
-      words = dictionary_to_words dictionary
+      words = lines_from_dictionary.reject { |word| word.nil? || word.empty? }
 
       if words.empty?
         raise EmptyDictionaryError
@@ -24,28 +23,25 @@ module BlueCherries
       words
     end
 
-    def dictionary_to_words(dictionary)
-      dictionary.reject { |word| word.nil? || word.empty? }
-    end
-
-    def dictionary_path
-      search_paths = [
-        __FILE__ + '/../../../dictionaries/',
-        Dir.pwd + '/',
-        ''
-      ]
-
-      found_search_path = search_paths.find do |search_path|
-        file_path = File.expand_path @path, search_path
-        File.exists? file_path
-      end
-
-      if found_search_path.nil?
+    def lines_from_dictionary
+      begin
+        File.readlines(dictionary_path).map(&:chomp)
+      rescue Errno::ENOENT
         raise MissingDictionaryError, "ERROR: The dictionary file '#{@path}' "\
                                       "doesn't exist."
       end
+    end
 
-      File.expand_path @path, found_search_path
+    def dictionary_path
+      full_paths = search_paths.map do |search_path|
+        File.expand_path @path, search_path
+      end
+
+      full_paths.find { |path| File.exists? path } || ''
+    end
+
+    def search_paths
+      [__FILE__ + '/../../../dictionaries/', Dir.pwd + '/', '']
     end
   end
 end
