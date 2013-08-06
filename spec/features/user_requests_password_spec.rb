9ac44f48840015f8c1b5b001b0a991819de28c00
linduxed <linduxed@gmail.com>
2013-08-06 15:01:47 +0200

    Remove 'let' for consistency.

--- spec/features/user_requests_password_spec.rb
+++ spec/features/user_requests_password_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'Requesting a password' do
   context 'without any arguments' do
-    let(:generated_passwords) { Executable.run.lines }
-
     it 'returns passwords with a default format' do
+      generated_passwords = Executable.run.lines
+
       expect(generated_passwords).to have(6).items
       generated_passwords.each do |password|
         expect(password).to be_longer_than(15)
