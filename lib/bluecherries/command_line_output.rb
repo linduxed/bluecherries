2af15ded802c7acce0e0590b3c8d7ccd43e8de8f
Mikael Muszynski <linduxed@gmail.com>
2014-01-27 21:18:40 +0100

    Only store options in initialize method
    All procedure except binding instance variables should be handled
    outside of initialize.

--- lib/bluecherries/command_line_output.rb
+++ lib/bluecherries/command_line_output.rb
@@ -4,18 +4,24 @@ module BlueCherries
       @unparsed_args = opts.fetch(:args, ARGV)
       @generator = opts.fetch(:generator, BlueCherries::PasswordGenerator.new)
       @amount_of_passwords = opts.fetch(:amount_of_passwords, 6)
-
-      min_password_length = opts.fetch(:min_password_length, 16)
-      algorithm = opts.fetch(:algorithm, BlueCherries::HeadAlgorithm)
-      dictionary = opts.fetch(:dict, BlueCherries::Dictionary.new('english'))
-      @algorithm = algorithm.new dictionary, min_password_length
+      @min_password_length = opts.fetch(:min_password_length, 16)
+      @algorithm_kind = opts.fetch(:algorithm_kind, BlueCherries::HeadAlgorithm)
+      @dictionary = opts.fetch(:dict, BlueCherries::Dictionary.new('english'))
     end
 
     def run
-      @amount_of_passwords.times { puts @generator.generate(@algorithm) }
+      @amount_of_passwords.times { puts @generator.generate(algorithm) }
     rescue => error
       $stderr.puts "ERROR: #{error.class} -- #{error.message}"
       exit 1
+    end
+
+    private
+
+    attr_reader :algorithm_kind, :dictionary, :min_password_length
+
+    def algorithm
+      algorithm_kind.new dictionary, min_password_length
     end
   end
 end
