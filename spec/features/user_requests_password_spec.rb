380e6c58e7a8cb2e2ed228a2eb1fc6d4604796c4
Mikael Muszynski <linduxed@gmail.com>
2014-04-10 09:14:38 +0200

    Remove unrelated pending tests
    These don't belong in this branch.

--- spec/features/user_requests_password_spec.rb
+++ spec/features/user_requests_password_spec.rb
@@ -13,29 +13,6 @@ describe 'Requesting a password' do
     end
   end
 
-  context 'with a valid non-default algorithm argument' do
-    it 'returns a list of passwords' do
-      pending 'Finish the invalid-flag test first.'
-
-      default_passwords = Executable.run.lines
-      generated_passwords = Executable.run('-a random').lines
-
-      expect(generated_passwords).to have(6).items
-      expect(generated_passwords).not_to eq default_passwords
-    end
-  end
-
-  context 'with an invalid algorithm argument' do
-    it 'exits with exit code "64" and prints an error message to $stderr' do
-      pending 'Finish the invalid-flag test first.'
-
-      executable = Executable.run('-a foobarAlgorithm')
-
-      expect(executable.exit_code).to eq 64
-      expect(executable.error).to have_error_message('non-existant algorithm')
-    end
-  end
-
   context 'with an invalid flag' do
     it 'exits with exit code "64" and prints an error message to $stderr' do
       executable = Executable.run('-foo someValidAlgorithm')
