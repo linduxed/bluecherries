c5224615f38560e0fde8023bbb32a9dc75c11af3
Mikael Muszynski <linduxed@gmail.com>
2014-04-13 20:28:50 +0200

    Add Random algorithm
    This algorithm picks elements randomly from the source dictionary.

--- lib/bluecherries.rb
+++ lib/bluecherries.rb
@@ -6,6 +6,7 @@ require 'bluecherries/layout'
 require 'bluecherries/motion'
 require 'bluecherries/password'
 require 'bluecherries/password_generator'
+require 'bluecherries/random_algorithm'
 require 'bluecherries/version'
 
 module BlueCherries
