f7af4d52e25c154cf0c035cddd405c8da142d74c
Mikael Muszynski <linduxed@gmail.com>
2014-04-18 11:12:38 +0200

    Remove redundant "begin" and "end"

--- spec/support/helpers/output_helpers.rb
+++ spec/support/helpers/output_helpers.rb
@@ -1,19 +1,15 @@
 def with_stubbed_stdout(&block)
-  begin
-    orig_stdout = $stdout
-    $stdout = StringIO.new
-    yield(block)
-  ensure
-    $stdout = orig_stdout
-  end
+  orig_stdout = $stdout
+  $stdout = StringIO.new
+  yield(block)
+ensure
+  $stdout = orig_stdout
 end
 
 def with_stubbed_stderr(&block)
-  begin
-    orig_stderr = $stderr
-    $stderr = StringIO.new
-    yield(block)
-  ensure
-    $stderr = orig_stderr
-  end
+  orig_stderr = $stderr
+  $stderr = StringIO.new
+  yield(block)
+ensure
+  $stderr = orig_stderr
 end
