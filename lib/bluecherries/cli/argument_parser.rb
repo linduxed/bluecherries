d1fa2bb9a2accbf109a3d99f1f06776b18ecc00e
Mikael Muszynski <linduxed@gmail.com>
2014-04-21 18:44:55 +0200

    Put ArgumentParser into BlueCherries module

--- lib/bluecherries/cli/argument_parser.rb
+++ lib/bluecherries/cli/argument_parser.rb
@@ -1,42 +1,44 @@
 require 'optparse'
 
-class ArgumentParser
-  def initialize(unparsed_args)
-    @unparsed_args = unparsed_args
-  end
-
-  def parse
-    opt_hash = {}
-
-    parser = OptionParser.new do |opts|
-      opts.banner = 'Usage: bluecherries [options]'
-
-      opts.on('-n [NUMBER]', Integer,
-        'Generate NUMBER passwords') do |n|
-        opt_hash[:amount_of_passwords] = n
-      end
-
-      opts.on('-l [NUMBER]', Integer,
-        'At least NUMBER characters') do |n|
-        opt_hash[:min_password_length] = n
-      end
-
-      opts.on('-d [DICTIONARY]',
-        'Absolute/relative path or default dicts.') do |n|
-        opt_hash[:dictionary_path] = n
-      end
+module BlueCherries
+  class ArgumentParser
+    def initialize(unparsed_args)
+      @unparsed_args = unparsed_args
     end
 
-    parser.parse(unparsed_args)
+    def parse
+      opt_hash = {}
 
-    opt_hash
-  rescue OptionParser::InvalidOption => error
-    $stderr.puts "ERROR: #{error}"
-    $stderr.puts parser.banner
-    exit 64
+      parser = OptionParser.new do |opts|
+        opts.banner = 'Usage: bluecherries [options]'
+
+        opts.on('-n [NUMBER]', Integer,
+          'Generate NUMBER passwords') do |n|
+          opt_hash[:amount_of_passwords] = n
+        end
+
+        opts.on('-l [NUMBER]', Integer,
+          'At least NUMBER characters') do |n|
+          opt_hash[:min_password_length] = n
+        end
+
+        opts.on('-d [DICTIONARY]',
+          'Absolute/relative path or default dicts.') do |n|
+          opt_hash[:dictionary_path] = n
+        end
+      end
+
+      parser.parse(unparsed_args)
+
+      opt_hash
+    rescue OptionParser::InvalidOption => error
+      $stderr.puts "ERROR: #{error}"
+      $stderr.puts parser.banner
+      exit 64
+    end
+
+    private
+
+    attr_reader :unparsed_args
   end
-
-  private
-
-  attr_reader :unparsed_args
 end
--- spec/lib/bluecherries/cli/argument_parser_spec.rb
+++ spec/lib/bluecherries/cli/argument_parser_spec.rb
@@ -1,49 +1,51 @@
 require 'spec_helper'
 
-describe ArgumentParser do
-  describe '#parse' do
-    describe 'flags' do
-      it 'adds amount_of_passwords to the output hash for the "-n" flag' do
-        args = %w[-n 3]
-        expected_hash = { amount_of_passwords: 3 }
+module BlueCherries
+  describe ArgumentParser do
+    describe '#parse' do
+      describe 'flags' do
+        it 'adds amount_of_passwords to the output hash for the "-n" flag' do
+          args = %w[-n 3]
+          expected_hash = { amount_of_passwords: 3 }
 
-        parsed_arguments = ArgumentParser.new(args).parse
+          parsed_arguments = ArgumentParser.new(args).parse
 
-        expect(parsed_arguments).to eq(expected_hash)
+          expect(parsed_arguments).to eq(expected_hash)
+        end
+
+        it 'adds min_password_length to the output hash for the "-l" flag' do
+          args = %w[-l 20]
+          expected_hash = { min_password_length: 20 }
+
+          parsed_arguments = ArgumentParser.new(args).parse
+
+          expect(parsed_arguments).to eq(expected_hash)
+        end
+
+        it 'adds dictionary_path to the output hash for the "-d" flag' do
+          file = Tempfile.new 'dictionary_file'
+          file.write "foo\nbar\nbaz\n"
+          file.close
+          args = ['-d', file.path]
+          expected_hash = { dictionary_path: file.path }
+
+          parsed_arguments = ArgumentParser.new(args).parse
+
+          expect(parsed_arguments).to eq(expected_hash)
+        end
       end
 
-      it 'adds min_password_length to the output hash for the "-l" flag' do
-        args = %w[-l 20]
-        expected_hash = { min_password_length: 20 }
+      it 'prints a usage message if an invalid option is provided' do
+        with_stubbed_stderr do
+          bad_args = %w[-foo bar]
 
-        parsed_arguments = ArgumentParser.new(args).parse
+          expect do
+            ArgumentParser.new(bad_args).parse
+          end.to terminate.with_code(64)
 
-        expect(parsed_arguments).to eq(expected_hash)
-      end
-
-      it 'adds dictionary_path to the output hash for the "-d" flag' do
-        file = Tempfile.new 'dictionary_file'
-        file.write "foo\nbar\nbaz\n"
-        file.close
-        args = ['-d', file.path]
-        expected_hash = { dictionary_path: file.path }
-
-        parsed_arguments = ArgumentParser.new(args).parse
-
-        expect(parsed_arguments).to eq(expected_hash)
-      end
-    end
-
-    it 'prints a usage message if an invalid option is provided' do
-      with_stubbed_stderr do
-        bad_args = %w[-foo bar]
-
-        expect do
-          ArgumentParser.new(bad_args).parse
-        end.to terminate.with_code(64)
-
-        expect($stderr.string).to have_error_message('invalid option')
-        expect($stderr.string).to match(/Usage:/)
+          expect($stderr.string).to have_error_message('invalid option')
+          expect($stderr.string).to match(/Usage:/)
+        end
       end
     end
   end
