b4d162b6c26e994243819aeba9513a8711b45cf6
Mikael Muszynski <linduxed@gmail.com>
2014-05-03 18:33:08 +0200

    Add acceptance test for two-argument scenario

--- spec/features/user_requests_a_password_with_arguments_spec.rb
+++ spec/features/user_requests_a_password_with_arguments_spec.rb
@@ -12,4 +12,16 @@ describe 'Requesting passwords' do
       end
     end
   end
+
+  context 'with both custom length and custom amount of passwords' do
+    it 'returns the correct amount of passwords with right length' do
+      generated_passwords = Executable.run('-l 50 -n 20').lines
+
+      expect(generated_passwords).not_to be_empty
+      expect(generated_passwords.count).to eq(20)
+      generated_passwords.each do |password|
+        expect(password).to be_longer_than(50)
+      end
+    end
+  end
 end
