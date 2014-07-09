1c21675f6060738cb814ed8e006dbfcaa9dc5897
Mikael Muszynski <linduxed@gmail.com>
2014-07-09 18:20:24 +0200

    Write algorithm test for different layouts

--- spec/lib/bluecherries/algorithms/typeable_algorithm_spec.rb
+++ spec/lib/bluecherries/algorithms/typeable_algorithm_spec.rb
@@ -13,7 +13,21 @@ module BlueCherries
         expect(password_components.join.length).to be >= min_length
       end
 
-      it 'gives different components for different keyboard layouts'
+      it 'gives different components for different keyboard layouts' do
+        qwerty_word = 'asdfkj'
+        colemak_word = 'arsten'
+        min_length = qwerty_word.length - 1
+        dictionary = Tempfile.new 'dictionary_file'
+        dictionary.write(qwerty_word + "\n" + colemak_word + "\n")
+        dictionary.close
+
+        qwerty_components = TypeableAlgorithm.new(dictionary, min_length,
+          qwerty_layout).create_password_components
+        colemak_components = TypeableAlgorithm.new(dictionary, min_length,
+          colemak_layout).create_password_components
+
+        expect(qwerty_components).not_to eq(colemak_components)
+      end
     end
   end
 end
