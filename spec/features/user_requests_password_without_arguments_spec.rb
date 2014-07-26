64a71af5933dc9c5879b8d7d8dc2462ef27c096a
Mikael Muszynski <linduxed@gmail.com>
2014-07-27 00:56:02 +0200

    Add rspec-collection_matchers gem
    RSpec 3 needs it for the `have` matchers.

--- spec/features/user_requests_password_without_arguments_spec.rb
+++ spec/features/user_requests_password_without_arguments_spec.rb
@@ -5,7 +5,7 @@ describe 'Requesting a password' do
     it 'returns passwords with a default format' do
       generated_passwords = Executable.run.lines
 
-      expect(generated_passwords).to have(6).items
+      expect(generated_passwords).to have_exactly(6).items
       generated_passwords.each do |password|
         expect(password).to be_longer_than(15)
         expect(password).to have_camel_case
