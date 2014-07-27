0b1508ec4d61d44487a4e1b4629aeb2c619cbac2
Mikael Muszynski <linduxed@gmail.com>
2014-07-27 13:42:43 +0200

    Don't use deprecated RSpec pending block syntax

--- spec/features/user_requests_a_typable_password_spec.rb
+++ spec/features/user_requests_a_typable_password_spec.rb
@@ -3,22 +3,21 @@ require 'spec_helper'
 describe 'Requesting typeable passwords' do
   context 'using the QWERTY layout with a polarized dictionary' do
     it 'returns passwords with only the highly typeable words' do
-      pending 'Fix TypeableAlgorithm tests first' do
-        high_typability_words = %w[asdflkj qwerpoiu jklfds sdfjkl oiurew]
-        low_typeability_words = %w[pzecuq yqmebigs mqoxht]
-        all_words = (high_typability_words + low_typeability_words).join("\n")
-        dictionary = Tempfile.new 'dictionary_file'
-        dictionary.write(all_words + "\n")
-        dictionary.close
+      pending 'Fix TypeableAlgorithm tests first'
+      high_typability_words = %w[asdflkj qwerpoiu jklfds sdfjkl oiurew]
+      low_typeability_words = %w[pzecuq yqmebigs mqoxht]
+      all_words = (high_typability_words + low_typeability_words).join("\n")
+      dictionary = Tempfile.new 'dictionary_file'
+      dictionary.write(all_words + "\n")
+      dictionary.close
 
-        executable = Executable.run("-a typeable -d #{dictionary.path}")
-        generated_passwords = executable.lines
+      executable = Executable.run("-a typeable -d #{dictionary.path}")
+      generated_passwords = executable.lines
 
-        expect(executable.error).to be_empty, "Error:\n#{executable.error}"
-        generated_passwords.each do |password|
-          expect(password).to include_words_from(high_typability_words)
-          expect(password).not_to include_words_from(low_typeability_words)
-        end
+      expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+      generated_passwords.each do |password|
+        expect(password).to include_words_from(high_typability_words)
+        expect(password).not_to include_words_from(low_typeability_words)
       end
     end
   end
