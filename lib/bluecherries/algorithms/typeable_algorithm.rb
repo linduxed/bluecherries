84b703fba63e9df1737637408ee439c7de8bf51a
Mikael Muszynski <linduxed@gmail.com>
2014-08-31 18:36:14 +0200

    Make TypeableAlgorithm resemble HeadAlgorithm
    Main structure replicated with minor changes.
    LayoutBasedDictionarySorter referenced but not implemented.

--- lib/bluecherries/algorithms/typeable_algorithm.rb
+++ lib/bluecherries/algorithms/typeable_algorithm.rb
@@ -1,11 +1,39 @@
 module BlueCherries
   # Finds words that are easy to type for specific keyboard layouts
   class TypeableAlgorithm
-    def initialize(dictionary, min_length)
+    def initialize(dictionary, min_length, layout)
+      @dictionary = dictionary
+      @min_length = min_length
+      @layout = layout
     end
 
     def create_password_components
-      ['1234567']
+      password_components = []
+
+      while password_components.join.length < min_length
+        password_components << next_word
+      end
+
+      password_components
+    end
+
+    private
+
+    attr_reader :dictionary, :min_length, :layout
+
+    def next_word
+      sorted_dictionary_enumerator.next
+    rescue StopIteration
+      sorted_dictionary.first
+    end
+
+    def sorted_dictionary_enumerator
+      @sorted_dictionary_enumerator ||= sorted_dictionary.to_enum
+    end
+
+    def sorted_dictionary
+      @sorted_dictionary ||= LayoutBasedDictionarySorter.new(
+        dictionary, layout).sort
     end
   end
 end
