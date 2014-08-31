c4bbc678fa45885c367e69423d8f3c066bf3fc4a
Mikael Muszynski <linduxed@gmail.com>
2014-08-31 09:24:20 +0200

    Add `Algorithm.names_of_all_algorithms`
    This also necessitated the addition of `Algorithm.name`, which is mostly
    used on classes inheriting from Algorithm (`Algorithm.save` returns an
    empty string, so not very useful).

--- lib/bluecherries/algorithms/algorithm.rb
+++ lib/bluecherries/algorithms/algorithm.rb
@@ -1,3 +1,11 @@
 module BlueCherries
-  class Algorithm; end
+  class Algorithm
+    def self.names_of_all_algorithms
+      descendants.map(&:name)
+    end
+
+    def self.name
+      to_s.gsub(/BlueCherries::/, '').gsub(/Algorithm/, '').downcase
+    end
+  end
 end
