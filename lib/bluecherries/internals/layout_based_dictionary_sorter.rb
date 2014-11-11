0c431df12461f39d5f922654de558c48be07b690
Mikael Muszynski <linduxed@gmail.com>
2014-11-11 09:50:54 +0100

    Implement LayoutBasedDictionarySorter
    This patch provides the remaining implementation of
    LayoutBasedDictionarySorter, up until now a skeleton class.
    
    The MotionAnalyzedWord class is included in the same file as
    LayoutBasedDictionarySorter because it's functionality that's so closely
    tied to it that it could almost have been implemented inside
    LayoutBasedDictionarySorter.

--- lib/bluecherries/internals/layout_based_dictionary_sorter.rb
+++ lib/bluecherries/internals/layout_based_dictionary_sorter.rb
@@ -1,4 +1,6 @@
 module BlueCherries
+  class NonExhaustiveMotionSetError < StandardError; end
+
   class LayoutBasedDictionarySorter
     def initialize(dictionary, layout)
       @dictionary = dictionary
@@ -6,11 +8,64 @@ module BlueCherries
     end
 
     def sort
-      []
+      motion_analyzed_words.sort_by { |word| word.value }.map(&:to_s)
     end
 
     private
 
     attr_reader :dictionary, :layout
+
+    def motion_analyzed_words
+      dictionary.words.map { |word| MotionAnalyzedWord.new(word, layout) }
+    end
+  end
+
+  class MotionAnalyzedWord
+    def initialize(word, layout)
+      @word = word
+      @layout = layout
+    end
+
+    def to_s
+      @word
+    end
+
+    def value
+      letters = @word.dup
+      matching_motions = []
+
+      while !letters.empty? do
+        match = find_highest_valued_matching_motion(letters)
+        remove_letters_from_start_of_word!(letters, match)
+
+        matching_motions << match
+      end
+
+      matching_motions.reduce(0) { |sum, motion| sum + motion.value }
+    end
+
+    private
+
+    attr_reader :layout
+
+    def find_highest_valued_matching_motion(word)
+      motions_sorted_descending_by_value.find(missing_motion) do |motion|
+        word.match(/^#{motion.letters}/)
+      end
+    end
+
+    def motions_sorted_descending_by_value
+      @sorted_motions ||= layout.motions.sort_by do |motion|
+        motion.value
+      end.reverse
+    end
+
+    def remove_letters_from_start_of_word!(word, motion)
+      word.sub!(/^#{motion.letters}/, '')
+    end
+
+    def missing_motion
+      -> { raise NonExhaustiveMotionSetError }
+    end
   end
 end
--- spec/lib/bluecherries/internals/layout_based_dictionary_sorter_spec.rb
+++ spec/lib/bluecherries/internals/layout_based_dictionary_sorter_spec.rb
@@ -3,7 +3,53 @@ require 'spec_helper'
 module BlueCherries
   describe LayoutBasedDictionarySorter do
     describe '#sort' do
-      it 'sorts'
+      it 'returns the words found in the provided dictionary' do
+        dictionary = double(:dictionary, words: %w[ foobar quuxbar ])
+        foo = double(:foo, value: 5, letters: 'foo')
+        quux = double(:quux, value: 1, letters: 'quux')
+        bar = double(:bar, value: 5, letters: 'bar')
+        layout = double(:layout, motions: [foo, quux, bar])
+
+        sorted_words = LayoutBasedDictionarySorter.new(dictionary, layout).sort
+
+        expect(dictionary).to have_received(:words)
+        expect(sorted_words).to match_array(dictionary.words)
+      end
+    end
+  end
+
+  describe MotionAnalyzedWord do
+    describe '#to_s' do
+      it 'returns the word provided upon instantiation' do
+        word = MotionAnalyzedWord.new('foobar', double(:layout)).to_s
+
+        expect(word).to eq('foobar')
+      end
+    end
+
+    describe '#value' do
+      it 'calculates the sum of the values of the motions of the word' do
+        analyzed_word = 'foobar'
+        foo = double(:foo, value: 5, letters: 'foo')
+        quux = double(:quux, value: 1, letters: 'quux')
+        bar = double(:bar, value: 5, letters: 'bar')
+        layout = double(:layout, motions: [foo, quux, bar])
+
+        value = MotionAnalyzedWord.new(analyzed_word, layout).value
+
+        expect(layout).to have_received(:motions)
+        expect(value).to eq(10)
+      end
+
+      it "raises an error if the available motions can't construct the word" do
+        abc = double(:abc, value: 1, letters: 'abc')
+        xyz = double(:xyz, value: 5, letters: 'xyz')
+        layout = double(:layout, motions: [abc, xyz])
+
+        expect do
+          MotionAnalyzedWord.new('foobar', layout).value
+        end.to raise_error(NonExhaustiveMotionSetError)
+      end
     end
   end
 end
