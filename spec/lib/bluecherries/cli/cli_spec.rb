d49c452ade1c87d7baa0a108e4c127acac02aa59
Mikael Muszynski <linduxed@gmail.com>
2015-06-19 17:32:46 +0200

    Style: redundant braces around hash parameters

--- spec/lib/bluecherries/cli/cli_spec.rb
+++ spec/lib/bluecherries/cli/cli_spec.rb
@@ -22,7 +22,7 @@ module BlueCherries
           allow(ArgumentParser).to receive(:new).and_return(parser)
           input_args = %w[-n 3]
 
-          CLI.new({ args: input_args }).run
+          CLI.new(args: input_args).run
 
           expect(ArgumentParser).to have_received(:new).with(input_args)
         end
@@ -38,7 +38,7 @@ module BlueCherries
 
           CLI.new(args_and_options).run
 
-          expect(CommandLineOutput).to have_received(:new).with({ foo: 'foo' })
+          expect(CommandLineOutput).to have_received(:new).with(foo: 'foo')
         end
       end
 
@@ -69,11 +69,11 @@ module BlueCherries
             allow(silent_output).to receive(:lines)
             allow(CommandLineOutput).to receive(:new).and_return(silent_output)
 
-            CLI.new({
+            CLI.new(
               args: 'whatever parses into { amount_of_passwords: 3 }',
               amount_of_passwords: 10,
               min_password_length: 10
-            }).run
+            ).run
 
             merged_options = {
               min_password_length: 10,
