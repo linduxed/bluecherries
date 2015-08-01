ebf3b1d66e4b762f889c3a7005b4d9c75e3d5e9f
Mikael Muszynski <linduxed@gmail.com>
2015-08-02 01:03:50 +0200

    Add positive test case for Layout#keys
    A test for what Layout#keys should actually do is missing. This patch
    fixes that.

--- spec/lib/bluecherries/internals/layout_spec.rb
+++ spec/lib/bluecherries/internals/layout_spec.rb
@@ -9,6 +9,47 @@ module BlueCherries
     end
 
     describe '#keys' do
+      context 'valid layout was provided' do
+        it 'returns a list of keys corresponding to the layout file' do
+          file = Tempfile.new 'layout_file'
+          file.write("qwertyuiop\nasdfghjkl\nzxcvbnm")
+          file.close
+
+          keys = Layout.new(file.path).keys
+
+          expect(keys).to include(
+            Key.new('q', 0, 0),
+            Key.new('w', 0, 1),
+            Key.new('e', 0, 2),
+            Key.new('r', 0, 3),
+            Key.new('t', 0, 4),
+            Key.new('y', 0, 5),
+            Key.new('u', 0, 6),
+            Key.new('i', 0, 7),
+            Key.new('o', 0, 8),
+            Key.new('p', 0, 9),
+            Key.new('a', 1, 0),
+            Key.new('s', 1, 1),
+            Key.new('d', 1, 2),
+            Key.new('f', 1, 3),
+            Key.new('g', 1, 4),
+            Key.new('h', 1, 5),
+            Key.new('j', 1, 6),
+            Key.new('k', 1, 7),
+            Key.new('l', 1, 8),
+            Key.new('z', 2, 0),
+            Key.new('x', 2, 1),
+            Key.new('c', 2, 2),
+            Key.new('v', 2, 3),
+            Key.new('b', 2, 4),
+            Key.new('n', 2, 5),
+            Key.new('m', 2, 6)
+          )
+
+          file.delete
+        end
+      end
+
       context 'malformed layout was provided' do
         it 'raises an error' do
           file = Tempfile.new 'layout_file'
